67e3f5a25858095bcdb2c600a3e65f6f6097c40f
Mayme Clayton <mayme.clayton@gmail.com>
2018-10-12 14:12:49 -0400

    fixes longest word logic

--- main.js
+++ main.js
@@ -32,18 +32,51 @@ let app = new Vue({
       this.word = this.enteredText.length;
 
       //number of sentences
-      this.sentence = this.enteredText.toString().split(/[\./||\?/||\!/]/g).length-1;
+      this.sentence = this.enteredText.toString().split(/[\./||\?/||\!/]/g).length;
 
       //number of paragraphs
       this.paragraph = this.enteredText.toString().split("\n\n").length;
 
       //longest word
-      this.longest = this.enteredText.sort((a,b) => {return a.length<b.length;})[0];
+      let noPunc=this.enteredText.toString().replace(/[,.?!]/g,' ');
+      let onlyWords=noPunc.split( " " );
+      let length=0;
+      let word = '';
+
+      for (let i =0; i<onlyWords.length; i++){
+        if(onlyWords[i].length>length){
+          length = onlyWords[i].length;
+          word = onlyWords[i];
+        }
+
+      }
+        this.longest = word;
 
       // most frequent character
+      let frequent = {};
+      let max = '';
+      let ar = this.enteredText.toString().replace(/\s/g, " ");
+
+      for (let i = 0; i < ar.length; i++){
+        let key =this.enteredText.toString().replace(/\s/g, " ")[i];
+
+        if (!frequent[key]){
+          frequent[key]=1;
+        }
+
+        else {
+          frequent[key] ++;
+          if(max==''|| frequent[key] > frequent[max]){
+            max=key;
+            this.freqChar = max;
+          }
+        }
+      }
+
       //this.freqChar = this.enteredText
 
-      //clears out the input field when button is clicked.
+
+      //clears out the input field when button is clicked. Not sure if I want this.
       //this.inputData = "";
 
     }
